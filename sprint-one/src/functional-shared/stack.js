20c51786b17643818f51c005b565e93c7a73efab
Harry Clemente <hclemente1989@gmail.com>
2020-10-07 16:17:10 -0700

    Added empty methods for Stack class and extend method

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  newStack.size = 0;
+  extend(Stack, stackMethods);
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var stackMethods = {};
 
+Stack.stackMethods.push = function(value) {
 
+};
+
+Stack.stackMethods.pop = function(value) {
+
+};
+
+Stack.stackMethods.size = function(value) {
+
+};
