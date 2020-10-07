5c9210a8682eca46deb66c5b8af1316b2999b8f1
Harry Clemente <hclemente1989@gmail.com>
2020-10-07 15:23:22 -0700

    Implemented functional stack.js

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,24 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var size = someInstance.size(storage);
+    storage[size] = value;
   };
 
   someInstance.pop = function() {
+    var size = someInstance.size(storage);
+
+    removedKey = storage[size - 1];
+    delete storage[size - 1];
+    return removedKey;
   };
 
   someInstance.size = function() {
+    var size = 0;
+    for (var key in storage) {
+      size++;
+    }
+    return size;
   };
 
   return someInstance;
