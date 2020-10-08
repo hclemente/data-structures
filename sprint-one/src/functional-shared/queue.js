a1cecd94dcf54f0a38df5f3bb8c8e2b18febb359
Harry Clemente <hclemente1989@gmail.com>
2020-10-07 23:22:49 -0700

    Implemented size() for queue.js in function shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,27 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queueStack = {};
+  extend(queueStack, queueMethods);
+
+  return queueStack;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function (value) {
+  this[this.size()] = value;
+};
+
+queueMethods.dequeue = function (value) {
+
+};
+
+queueMethods.size = function () {
+  var size = 0;
+  for (var key in this) {
+    size++;
+  }
+  return size - 3;
+};
 
