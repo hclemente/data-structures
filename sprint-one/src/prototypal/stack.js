60f43af5502c518a58dd321daf13cdd3a39d3683
Harry Clemente <hclemente1989@gmail.com>
2020-10-08 10:38:14 -0700

    Implementd stack.js for prototypical

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var newStack = Object.create(stackMethods);
+  return newStack;
+
 };
 
 var stackMethods = {};
 
+stackMethods.push = function (value) {
+  this[this.size()] = value;
+};
 
+stackMethods.pop = function () {
+  var removedKey = this[this.size() - 1];
+  delete this[this.size() - 1];
+  return removedKey;
+};
+
+stackMethods.size = function () {
+  var size = 0;
+  for (var key in this) {
+    if (!isNaN(key)) {
+      size++;
+    }
+  }
+  return size;
+};
