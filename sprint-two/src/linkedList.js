3a6b0c1f13f99a028d853cd3bc1425e16d5c8bbe
Harry Clemente <hclemente1989@gmail.com>
2020-10-08 17:05:09 -0700

    Completed Justification and Specification of interfaces (JSDocs comments)

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,20 +1,41 @@
-var LinkedList = function() {
-  var list = {};
-  list.head = null;
-  list.tail = null;
+/**
+ *
+ */
 
+var LinkedList = function() {
+  var list = {}; // this object holds the linked list: it is an array of Node objects + 2x references
+  list.head = null; // this reference points to the first Node object in the list
+  list.tail = null; // this reference points to the last Node object in the list
+
+  /**
+   * @param {any} value, the value which will be added to the end of the lined list
+   * @sideEffect modifies the linked list in-place
+   */
   list.addToTail = function(value) {
   };
 
+  /**
+   * @param none
+   * @returns {any} the value which was in the head node
+   * @sideEffect modifies the linked list in-place
+   */
   list.removeHead = function() {
   };
 
+  /**
+   * @param {any} target
+   * @returns {boolean} returns true if target is in a node in the linked list
+   */
   list.contains = function(target) {
   };
 
   return list;
 };
 
+/**
+ * @param {any} value
+ * @returns {Node}
+ */
 var Node = function(value) {
   var node = {};
 
